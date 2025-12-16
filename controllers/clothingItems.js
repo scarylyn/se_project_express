c705b0d7daa6b8c52450521c583793e1306d4f41
Scarylyn <scarylyn@gmail.com>
2025-12-16 04:06:52 -0500

    fixed up some promise chaining in the deleteItem function for real this time

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -42,7 +42,7 @@ const deleteItem = (req, res) => {
       if (item.owner._id !== user) {
         return res.status(403).send({ message: "Forbidden" });
       }
-      ClothingItem.findByIdAndDelete(itemId);
+      return ClothingItem.findByIdAndDelete(itemId);
     })
     .then(() => res.status(200).send({}))
     .catch((err) => {
