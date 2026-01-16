5cd2226e2038e67a9e5a3c298c2e6d4d7463ccc0
Scarylyn <scarylyn@gmail.com>
2026-01-16 05:04:26 -0500

    error handling baybeeee

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 mongoose
   .connect("mongodb://localhost:27017/wtwr_db")
@@ -18,8 +19,10 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
+app.use(requestLogger);
 app.use("/", mainRouter);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
