3c87b4f140c2ed0455e07dcab98081ae7724c678
Scarylyn <scarylyn@gmail.com>
2026-01-21 01:15:23 -0500

    ngl im sending this in for review even though I know its broken because I cant figure out how to fix it and ive been going in circles for an hour over one thing and im done asking ai for help and its too late at night to get any answers from humans so im just submitting it and hoping the reviewer doesnt cuss me out

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -5,22 +5,19 @@ module.exports.errorHandler = (err, req, res, next) => {
     console.error(err);
     const statusCode = err.statusCode || 400;
     const message = statusCode === 400 ? "Validation failed" : err.message;
-    res.status(statusCode).send(message);
-    next();
+    return res.status(statusCode).send(message);
   }
   if (err.name === "CastError") {
     console.error(err);
     const statusCode = err.statusCode || 400;
     const message =
       statusCode === 400 ? "The data provided is invalid" : err.message;
-    res.status(statusCode).send(message);
-    next();
+    return res.status(statusCode).send(message);
   }
   if (err.code === 11000) {
     const statusCode = err.statusCode || 409;
     const message = statusCode === 409 ? "Duplicate entry" : err.message;
-    res.status(statusCode).send(message);
-    next();
+    return res.status(statusCode).send(message);
   }
   return res.status(500).send({ message: "An error occured on the server" });
 };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -11,7 +11,7 @@ const { validateCardBody, validateId } = require("../middlewares/validation");
 
 router.post("/", auth, validateCardBody, createItem);
 
-router.get("/", validateId, getItems);
+router.get("/", validateCardBody, getItems);
 
 router.put("/:itemId/likes", auth, validateId, likeItem);
 
